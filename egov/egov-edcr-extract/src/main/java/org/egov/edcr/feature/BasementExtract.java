ae69fbe601513f137e29f1343e6f8ae8e3049497
Md Reza Khan <rezakhan9494@gmail.com>
2022-03-23 10:27:43 +0000

    Basement and report code fix

--- egov/egov-edcr-extract/src/main/java/org/egov/edcr/feature/BasementExtract.java
+++ egov/egov-edcr-extract/src/main/java/org/egov/edcr/feature/BasementExtract.java
@@ -14,57 +14,53 @@ import org.springframework.stereotype.Service;
 @Service
 public class BasementExtract extends FeatureExtract {
 
-    private static final Logger LOG = Logger.getLogger(BasementExtract.class);
-    @Autowired
-    private LayerNames layerNames;
+	private static final Logger LOG = Logger.getLogger(BasementExtract.class);
+	@Autowired
+	private LayerNames layerNames;
 
-    @Override
-    public PlanDetail extract(PlanDetail pl) {
-        Map<String, Integer> basementFeaturesColor = pl.getSubFeatureColorCodesMaster().get("Basement");
-        for (Block b : pl.getBlocks())
-            if (b.getBuilding() != null && b.getBuilding().getFloors() != null
-                    && !b.getBuilding().getFloors().isEmpty())
-                for (Floor f : b.getBuilding().getFloors())
-                    if (f.getNumber() == -1) {
-                        String basementFootPrint = layerNames.getLayerName("LAYER_NAME_BLOCK_NAME_PREFIX") + b.getNumber()
-                                + "_"+ layerNames.getLayerName("LAYER_NAME_LEVEL_NAME_PREFIX") + f.getNumber()
-                                + "_"+ layerNames.getLayerName("LAYER_NAME_BSMNT_FOOT_PRINT");
-                        /*
-                         * height from the floor to the soffit of the roof slab or ceiling
-                         */
-                        f.setHeightFromTheFloorToCeiling(Util.getListOfDimensionByColourCode(pl,
-                                String.format(basementFootPrint,
-                                        b.getNumber()),
-                                basementFeaturesColor.get(layerNames
-                                        .getLayerName("LAYER_NAME_HEIGHT_FROM_THE_FLOOR_TO_CEILING_COLOUR_CODE"))));
-                        
-                        /*
-                         * minimum height of the ceiling of upper basement above ground level
-                         */
-                        f.setHeightOfTheCeilingOfUpperBasement(Util.getListOfDimensionByColourCode(pl,
-                                String.format(basementFootPrint,
-                                        b.getNumber()),
-                                basementFeaturesColor.get(layerNames.getLayerName(
-                                        "LAYER_NAME_HEIGHT_OF_THE_CEILING_OF_UPPER_BASEMENT_COLOUR_CODE"))));
-                        
-                        /*
-                         * Level of basement under the ground
-                         */
-                        f.setLevelOfBasementUnderGround(Util.getListOfDimensionByColourCode(pl,
-                                String.format(basementFootPrint,
-                                        b.getNumber()),
-                                basementFeaturesColor.get(layerNames.getLayerName(
-                                        "LAYER_NAME_COLOUR_CODE_LEVEL_OF_BASEMENT_UNDER_GROUND"))));
+	@Override
+	public PlanDetail extract(PlanDetail pl) {
+		Map<String, Integer> basementFeaturesColor = pl.getSubFeatureColorCodesMaster().get("Basement");
+		for (Block b : pl.getBlocks())
+			if (b.getBuilding() != null && b.getBuilding().getFloors() != null
+					&& !b.getBuilding().getFloors().isEmpty())
+				for (Floor f : b.getBuilding().getFloors())
+					if (f.getNumber() == -1) {
+						String basementFootPrint = layerNames.getLayerName("LAYER_NAME_BLOCK_NAME_PREFIX")
+								//+ b.getNumber() + "_" + layerNames.getLayerName("LAYER_NAME_LEVEL_NAME_PREFIX")
+								+ b.getNumber() + "_" + layerNames.getLayerName("LAYER_NAME_FLOOR_NAME_PREFIX")
+								+ f.getNumber() + "_BLDG_FOOTPRINT";
+						/*
+						 * height from the floor to the soffit of the roof slab or ceiling
+						 */
+						f.setHeightFromTheFloorToCeiling(Util.getListOfDimensionByColourCode(pl,
+								String.format(basementFootPrint, b.getNumber()), basementFeaturesColor.get(layerNames
+										.getLayerName("LAYER_NAME_HEIGHT_FROM_THE_FLOOR_TO_CEILING_COLOUR_CODE"))));
 
-                    }
+						/*
+						 * minimum height of the ceiling of upper basement above ground level
+						 */
+						f.setHeightOfTheCeilingOfUpperBasement(
+								Util.getListOfDimensionByColourCode(pl, String.format(basementFootPrint, b.getNumber()),
+										basementFeaturesColor.get(layerNames.getLayerName(
+												"LAYER_NAME_HEIGHT_OF_THE_CEILING_OF_UPPER_BASEMENT_COLOUR_CODE"))));
 
-        return pl;
-    }
+						/*
+						 * Level of basement under the ground
+						 */
+						f.setLevelOfBasementUnderGround(Util.getListOfDimensionByColourCode(pl,
+								String.format(basementFootPrint, b.getNumber()), basementFeaturesColor.get(layerNames
+										.getLayerName("LAYER_NAME_COLOUR_CODE_LEVEL_OF_BASEMENT_UNDER_GROUND"))));
 
-    @Override
-    public PlanDetail validate(PlanDetail pl) {
+					}
 
-        return pl;
-    }
+		return pl;
+	}
+
+	@Override
+	public PlanDetail validate(PlanDetail pl) {
+
+		return pl;
+	}
 
 }
