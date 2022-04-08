7d58ba36b710a35811803f16fce97b4b7249ea8a
Md Reza Khan <rezakhan9494@gmail.com>
2022-04-08 10:23:04 +0000

    Flyway updated qry

--- egov/egov-commons/src/main/resources/db/migration/main/V20210713113046__commons_basement_ground_level_colorcode.sql
+++ egov/egov-commons/src/main/resources/db/migration/main/V20210713113046__commons_basement_ground_level_colorcode.sql
@@ -1,4 +1,6 @@
 insert into state.egdcr_layername(id,key,value,createdby,createddate,lastmodifiedby,lastmodifieddate,version) 
 select nextval('state.seq_egdcr_layername'),'LAYER_NAME_COLOUR_CODE_LEVEL_OF_BASEMENT_UNDER_GROUND','COLOUR_CODE_LEVEL_OF_BASEMENT_UNDER_GROUND',1,now(),1,now(),0 where not exists(select key from state.egdcr_layername where key='LAYER_NAME_COLOUR_CODE_LEVEL_OF_BASEMENT_UNDER_GROUND');
 
-INSERT INTO egdcr_sub_feature_colorcode(id, feature, subfeature, colorcode,ordernumber) VALUES (nextval('seq_egdcr_sub_feature_colorcode'), 'Basement', 'COLOUR_CODE_LEVEL_OF_BASEMENT_UNDER_GROUND', 3, 8);
+select setval('generic.seq_egdcr_sub_feature_colorcode', (select max(id) from generic.egdcr_sub_feature_colorcode ));
+
+INSERT INTO generic.egdcr_sub_feature_colorcode(id, feature, subfeature, colorcode,ordernumber) VALUES (nextval('generic.seq_egdcr_sub_feature_colorcode'), 'Basement', 'COLOUR_CODE_LEVEL_OF_BASEMENT_UNDER_GROUND', 3, 8);
