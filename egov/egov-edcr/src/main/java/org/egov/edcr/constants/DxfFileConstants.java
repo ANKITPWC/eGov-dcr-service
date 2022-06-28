71fd9d21404719301332a7f0dcdcd85f51d29cc7
Md Reza Khan <rezakhan9494@gmail.com>
2022-06-28 07:31:01 +0000

    error 2

--- egov/egov-edcr/src/main/java/org/egov/edcr/constants/DxfFileConstants.java
+++ egov/egov-edcr/src/main/java/org/egov/edcr/constants/DxfFileConstants.java
@@ -643,7 +643,7 @@ public class DxfFileConstants {
 	public static final String MAINRIVER = "MainRiver";
 	public static final String SUBRIVER = "SubRiver";
 
-	public static final Map<String, String> SERVICE_TYPE = new ConcurrentHashMap<>();
+	public static Map<String, String> SERVICE_TYPE = new ConcurrentHashMap<>();
 	static {
 		SERVICE_TYPE.put("NEW_CONSTRUCTION", "New Construction");
 		SERVICE_TYPE.put("ADDITION_AND_ALTERATION", "Addition and Alteration");
